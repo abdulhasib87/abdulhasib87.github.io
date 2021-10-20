9c5aaf8cc9cdf7cbead9f53c059485d99325ac95
abdulhasib <abdul.hasib.sadiqin@gmail.com>
2021-10-21 07:53:57 +0800

    update assignment 4

---
+++ assignment4/js/q4.js
@@ -0,0 +1,17 @@
+function checkPalindrome(str) {
+    const len = str.length;
+
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            alert("It is not a palindrome");
+            return;
+        }
+    }
+    alert("It is a palindrome");    
+}  
+
+let palBtn = document.getElementById("cPalindrome")
+palBtn.addEventListener("click", function(){
+    let getString = document.getElementById("nPalindrome")
+    checkPalindrome(getString.value)           
+})
